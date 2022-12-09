8009c609bd047e423804eb78416e40b795cebf9f
Paulo Arruda <paulo@parruda.net>
2022-12-09 07:42:35 -0400

    Load configuration from .reek.yml (#1)
    * Load configuration from .reek.yml
    
    The plugin is currently not loading the config from a `.reek.yml` you may have in your project. Looking at Reek's source I found the method `Reek::Configuration::ConfigurationFileFinder.find_and_load`.
    
    * Update version.rb
    
    * Update solargraph-reek.rb

--- lib/solargraph-reek.rb
+++ lib/solargraph-reek.rb
@@ -6,7 +6,8 @@ module Solargraph
   module Reek
     class Reporter < Solargraph::Diagnostics::Base
       def diagnose source, _api_map
-        examiner = ::Reek::Examiner.new(source.code.dup)
+        configuration = ::Reek::Configuration::AppConfiguration.from_default_path
+        examiner = ::Reek::Examiner.new(source.code.dup, configuration: configuration)
         examiner.smells.map { |w| warning_to_diagnostic(w) }
       rescue ::Reek::Errors::SyntaxError
         []
